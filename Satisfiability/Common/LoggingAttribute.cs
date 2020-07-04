917711a249a80a7a2de5f9d401ac520177fc4f81
Harald Wilbertz <Harald.Wilbertz@gmx.de>
2020-07-04 20:58:19 +0200

    Logging helper added.

---
+++ Satisfiability/Common/LoggingAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Remoting.Contexts;
+using System.Runtime.Remoting.Activation;
+
+namespace Satisfiability.Common
+{
+    /// <summary>
+    /// This attribute is used to add logging functionality in an 
+    /// unobtrusive way. The class decorated with this attribute has 
+    /// to be derived from a context bound object. Since this attribute 
+    /// is a context attribute, the context properties have to be added 
+    /// during a call to GetPropertiesForNewContext.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class LoggingAttribute : ContextAttribute
+    {
+        /// <summary>
+        /// Default constructor, delegates work to base class implementation.
+        /// </summary>
+        public LoggingAttribute() : base("Logging")
+        {
+        }
+
+        public override void GetPropertiesForNewContext(IConstructionCallMessage ccm)
+        {
+            ccm.ContextProperties.Add(new LoggingProperty());
+        }
+
+        public override bool IsContextOK(Context ctx, IConstructionCallMessage ctorMsg)
+        {
+            return false;
+        }
+    }
+}
---
+++ Satisfiability/Common/LoggingProperty.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting.Contexts;
+
+namespace Satisfiability.Common
+{
+    public class LoggingProperty : IContextProperty, IContributeObjectSink
+    {
+        public IMessageSink GetObjectSink(MarshalByRefObject o, IMessageSink next)
+        {
+            return new LoggingAspect(next);
+        }
+
+        public string Name => "LoggingProperty";
+
+        public void Freeze(Context newContext)
+        {
+            // Nothing to do.
+        }
+
+        public bool IsNewContextOK(Context newCtx)
+        {
+            return true;
+        }
+    }
+}
