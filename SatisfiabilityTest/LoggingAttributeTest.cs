197afbebdf45d3ef374b929156919ea8b9b680dc
Harald Wilbertz <harald.wilbertz@gmx.de>
2020-11-21 20:05:45 +0100

    Empty unit test foe LoggingAttribute added.

---
+++ SatisfiabilityTest/LoggingAttributeTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLog;
+
+namespace SatisfiabilityTest
+{
+    /// <summary>
+    /// This is a test class for LoggingAttribute and is intended
+    /// to contain all LoggingAttribute Unit Tests
+    /// </summary>
+    [TestClass]
+    public class LoggingAttributeTest
+    {
+        #region Fields and Properties -----------------------------------------
+
+        /// <summary>
+        /// Gets or sets the test context which provides
+        /// information about and functionality for the current test run.
+        /// </summary>
+        public TestContext TestContext { get; set; }
+
+        Logger _logger = LogManager.GetCurrentClassLogger();
+
+        #endregion
+    }
+}
